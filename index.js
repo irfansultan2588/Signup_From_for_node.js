6625f764d39276ac025d60cdf540fb3d067e11e0
irfan <irfansultan201@gmail.com>
2022-08-18 19:53:47 +0500

    update

---
+++ index.js
@@ -0,0 +1,62 @@
+
+function signup() {
+
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var repeatPassword = document.getElementById('repeatPassword').value;
+
+    if (password !== repeatPassword) {
+        document.querySelector("#message").innerHTML = 'Passwords do not match, please try again';
+        return;
+    }
+
+    axios.post('https://my-signup-and-login-server.herokuapp.com/signup', {
+
+        firstName,
+        lastName,
+        email,
+        password,
+
+    })
+        .then(function (response) {
+            console.log(response.data);
+            document.querySelector("#message").innerHTML = response.data.message;
+        })
+        .catch(function (error) {
+            console.log(error.response.data);
+            document.querySelector("#message").innerHTML = error.response.data.message;
+        });
+}
+
+
+
+
+
+
+
+// LOGIN CODE///
+
+function login() {
+
+    // e.preventDefault();
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    debugger
+    axios.post('https://my-signup-and-login-server.herokuapp.com/login', {
+
+        email,
+        password,
+
+    })
+        .then(function (response) {
+            console.log(response.data);
+            document.querySelector("#message2").innerHTML = response.data.message;
+        })
+        .catch(function (error) {
+            console.log(error.response.data);
+            document.querySelector("#message2").innerHTML = error.response.data.message;
+        });
+}
